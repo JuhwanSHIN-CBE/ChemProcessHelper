7b89020b76bdf3c66471b1265d3a847238207183
SJH <papagos@snu.ac.kr>
2020-04-24 15:52:38 +0900

    파일 최초 생성

---
+++ ChemicalProcessHelper.hpp
@@ -0,0 +1,15 @@
+/*
+ChemicalProcessHelper.hpp
+복잡한 화학 공정에서의 계산을 편리하게 하기 위함.
+
+주요 클래스: ChemBase, ProcObjBase, MixerBase, RxtorBase, SpliterBase, StreamBase
+ProcObjBase
+    <= MixerBase, RxtorBase, SpliterBase
+*/
+
+#ifndef _CHEMPROCHELPER
+#define _CHEMPROCHELPER
+
+
+
+#endif
